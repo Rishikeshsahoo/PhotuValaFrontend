2ba180c188773c2159c0a4099ec6c9cc8b675d5e
Rishikesh <rishikeshsahoo2828@gmail.com>
2023-02-08 21:56:37 +0530

    testing3

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
   
   return (
     <div >
-      <BrowserRouter basename='/PhotuValaFrontend'>
+      <HashRouter>
         <Routes>
 
           {/* Home */}
@@ -26,7 +26,7 @@ const App = () => {
           {/* User  */}
           <Route path="/UserApp/*" element={<UserApp />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 };
