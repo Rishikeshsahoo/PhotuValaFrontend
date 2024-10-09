f79bf54172e1daa7e39aa034e6196bdc8e3a906b
Rishikesh <rishikeshsahoo2828@gmail.com>
2024-10-10 04:38:44 +0530

    removed useless code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { BrowserRouter, Routes, Route,HashRouter } from "react-router-dom";
+import React from "react";
+import {  Routes, Route,HashRouter } from "react-router-dom";
 import AdminApp from "./AdminApp"
 import UserApp from "./UserApp"
 import "./App.css";
